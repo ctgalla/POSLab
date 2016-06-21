0db64b6ecf6baabab44b14db850abe2636e464dd
Brandon Tabaczynski <brandon.tabaczynski@gmail.com>
2016-06-21 18:25:31 -0400

    FileI/O w/o switch, to update changes made to overall structure.

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,23 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Main {
 	public static void main(String[] args) {
-				Path productsPath = Paths.get("./products.csv");
+				Scanner scan = new Scanner(System.in);
+				Path productsPath = Paths.get("products.csv");
 				File productsFile = productsPath.toFile();
-				
 				ArrayList<Product> productList = new ArrayList<Product> ();
 				
 				try {            
 				    FileReader r = new FileReader(productsFile);
-				    BufferedReader in = new BufferedReader( r);
+				    BufferedReader in = new BufferedReader(r);
 				    String line = in.readLine();
 			    		while (line !=null)
 			    		{
-			        		String[] details = line.split("\t");
-			        		
+			        		String[] details = line.split(",");
 			        		Product temp = new Product(details[0], details[1], details[2], details[3]);
 			        		productList.add(temp);
 			        		
@@ -31,20 +31,34 @@ public class Main {
 			    		in.close();
 				 }  catch (IOException e) {
 				        System.out.println(e); 
-				 } 
-				
-				//output everything
-				for (Product p: productList) {
-					System.out.println(p);
-				}
-				
-				System.out.println("Produce only:");
-				
-				//one category
-				for (Product p: productList) {
-					if (p.getCategory().equals("Produce"))
-						System.out.println(p);
-				}
-
+				        int choice = scan.nextInt();
+				        switch(choice) {
+				        case 1:
+				        	break;
+				        case 2:
+				        	break;
+				        case 3:
+				        	break;
+				        case 4:
+				        	break;
+				        case 5:
+				        	break;
+				        case 6:
+				        	break;
+				        case 7:
+				        	break;
+				        case 8:
+				        	break;
+				        case 9:
+				        	break;
+				        case 10:
+				        	break;
+				        case 11:
+				        	break;
+				        case 12:
+				        	break;
+			 }
+		} 
 	}
 }
+
