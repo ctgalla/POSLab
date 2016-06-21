7489c3751ed09e6c3c052bb22c50d040dbde8c57
ctgalla <ctaylor.gallagher@gmail.com>
2016-06-21 16:07:18 -0400

    Receipt class, product incomplete, demo for receipt test

---
+++ src/Product.java
@@ -0,0 +1,14 @@
+
+public class Product {
+
+	public double getPrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getQuantity() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
