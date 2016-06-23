4deffae3f10b47f988af0d203b62079f5c82d33d
Brandon Tabaczynski <brandon.tabaczynski@gmail.com>
2016-06-22 23:15:05 -0400

    Working draft thus far......

--- src/Check.java
+++ src/Check.java
@@ -1,17 +1,6 @@
 
 public interface Check {
-	String checkNumber;
-	
-		public Check (String checkNumber){
-			this.checkNumber= checkNumber;
-		}
-
-		public String getCheckNumber() {
-			return checkNumber;	
-		}
-	
-	
-		public String toString() {
-			return "Check: " + checkNumber +"\nCheck Amount:" + Receipt.getGrandTotal();
-		}
+		void setCheckNumber(String checkNumber);
+		String getCheckNumber();
 }
+		
--- src/Validator.java
+++
@@ -1,9 +0,0 @@
-
-public class Validator {
-	
-//	public boolean isValidProduct(int productChoice) {
-//		// to check if 
-//		return !productChoice <= //<ProductList> [i];
-//	}
-}
-
